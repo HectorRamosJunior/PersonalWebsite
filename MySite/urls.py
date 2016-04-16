d1d19d81b7b75da4939acd6f73111ec1acda1776
HectorRamosJunior <HectorRamos.Junior@gmail.com>
2016-04-16 02:33:46 -0400

    urlpatterns changes

--- MySite/urls.py
+++ MySite/urls.py
@@ -17,8 +17,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include('website.urls', namespace="website") ),
     url(r'^todo/', include('todo.urls', namespace="todo") ),
     url(r'^AggroEats/', include('AggroEats.urls', namespace="AggroEats") ),
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('website.urls', namespace="website") ),
 ]
