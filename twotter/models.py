80be56d9b0a532b02a84d7aff6593cd69085270a
HectorRamosJunior <HectorRamos.Junior@gmail.com>
2016-05-06 22:11:02 -0400

    Removed retwoot count for now

--- twotter/models.py
+++ twotter/models.py
@@ -31,7 +31,7 @@ class Twoot(models.Model):
     creation_date = models.DateTimeField(auto_now_add=True)
 
     favorite_count = models.IntegerField(default=0)
-    retwoot_count = models.IntegerField(default=0)
+    #retwoot_count = models.IntegerField(default=0)
 
     text = models.CharField(max_length=140, null=False, blank=False)
 
